fca0c566721f37454b414e17bf9f658203632b35
Connor Burke <connor.p.b@gmail.com>
2025-03-28 01:00:02 -0400

    Additional examples

--- src/index.ts
+++ src/index.ts
@@ -20,18 +20,21 @@ function async2(bool2: boolean) {
     return errAsync("terrible")
 }
 
-const test1 = (bool: boolean) => sync1(bool).andThen(result => sync2(result)) // not callable
+const test1 = (bool: boolean) => sync1(bool).andThen(result => sync2(result)) // this expression is not callable - ts(2349)
 const test2 = (bool: boolean) => (sync1(bool) as Result<boolean, "bad">).andThen(result => sync2(result)) // succeeds on assertion
-const test3 = (bool: boolean) => sync1(bool).andThen<Result<boolean, "bad">>(result => sync2(result)) // not callable
-const test4 = (bool: boolean) => sync1(bool).andThen<boolean, "bad">(result => sync2(result)) // not callable
+const test3 = (bool: boolean) => sync1(bool).andThen<Result<boolean, "bad">>(result => sync2(result)) // this expression is not callable - ts(2349)
+const test4 = (bool: boolean) => sync1(bool).andThen<boolean, "bad">(result => sync2(result)) // this expression is not callable - ts(2349)
 
 const sync1withResultTypeAnnotation: (bool: boolean) => Result<boolean, "bad"> = (bool: boolean) => sync1(bool)
 const test5 = (bool: boolean) => sync1withResultTypeAnnotation(bool).andThen(result => sync2(result)) // succeeds when annotated
 
-const asyncTest1 = (bool: boolean) => async1(bool).andThen(bool2 => async2(bool2)) // not callable
+const test6 = (bool: boolean) => sync1(bool).asyncAndThen(result => async2(result)) // callable, but result is "any". Async inferred type is correct, ResultAsync<number, string>
+const test7 = (bool: boolean) => sync1(bool).asyncAndThen<number, string>(result => async2(result)) // callable, but result is "any". Annotated type matches inferred type above.
+
+const asyncTest1 = (bool: boolean) => async1(bool).andThen(bool2 => async2(bool2)) // this expression is not callable - ts(2349)
 const asyncTest2 = (bool: boolean) => (async1(bool) as ResultAsync<boolean, "bad">).andThen(bool2 => async2(bool2)) // succeeds
-const asyncTest3 = (bool: boolean) => async1(bool).andThen<ResultAsync<boolean, "bad">>(result => sync2(result)) // not callable
-const asyncTest4 = (bool: boolean) => async1(bool).andThen<boolean, "bad">(result => sync2(result)) // not callable
+const asyncTest3 = (bool: boolean) => async1(bool).andThen<ResultAsync<boolean, "bad">>(result => sync2(result)) // this expression is not callable - ts(2349)
+const asyncTest4 = (bool: boolean) => async1(bool).andThen<boolean, "bad">(result => sync2(result)) // this expression is not callable - ts(2349)
 
 const async1withResultTypeAnnotation: (bool: boolean) => ResultAsync<boolean, string> = (bool: boolean) => async1(bool)
 const asyncTest5 = (bool: boolean) => async1withResultTypeAnnotation(bool).andThen(result => sync2(result)) // succeeds when annotated
